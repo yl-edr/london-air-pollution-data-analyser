260d5a9b4585e7fad198c86a940f711fac7cd51c
Anton Davidouski <antdav2005@gmail.com>
2025-03-04 01:40:16 +0000

    base map colouring functionality implemented

--- MainMenu.java
+++ MainMenu.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -64,143 +65,142 @@ public class MainMenu extends Application {
         gridDataTab.setContent(gridContent);
 
         tabPane.getTabs().addAll(homeTab, mapViewTab, statsTab, gridDataTab);
-            MapImage map = new MapImage("resources/London.png");
-            map.testPixelAltering();
-            Image mapImage = map.getImage();
-            ImageView mapView = new ImageView(mapImage);
-            mapView.setPreserveRatio(true);
-            mapView.setSmooth(true);
-            mapView.setFitWidth(500);
+        MapImage map = new MapImage("resources/London.png");
+        Image mapImage = map.getImage();
+        ImageView mapView = new ImageView(mapImage);
+        mapView.setPreserveRatio(true);
+        mapView.setSmooth(true);
+        mapView.setFitWidth(500);
 
-            AnchorPane anchorPane = new AnchorPane();
-            anchorPane.getChildren().add(mapView);
-            anchorPane.setMinWidth(500);
-            anchorPane.setMinHeight(300);
-            mapView.fitWidthProperty().bind(anchorPane.widthProperty());
-            mapView.fitHeightProperty().bind(anchorPane.heightProperty());
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.getChildren().add(mapView);
+        anchorPane.setMinWidth(500);
+        anchorPane.setMinHeight(300);
+        mapView.fitWidthProperty().bind(anchorPane.widthProperty());
+        mapView.fitHeightProperty().bind(anchorPane.heightProperty());
 
-            BorderPane borderPane = new BorderPane();
-            borderPane.setCenter(anchorPane);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(anchorPane);
 
-            HBox bottomBar = new HBox(10);
-            bottomBar.getChildren().add(new Label("Test"));
-            bottomBar.setPrefHeight(50);
-            bottomBar.setMinHeight(30);
-            bottomBar.setMaxHeight(80);
+        HBox bottomBar = new HBox(10);
+        bottomBar.getChildren().add(new Label("Test"));
+        bottomBar.setPrefHeight(50);
+        bottomBar.setMinHeight(30);
+        bottomBar.setMaxHeight(80);
 
-            GridPane rightBar = new GridPane();
-            rightBar.setPadding(new Insets(10));
-            rightBar.setPrefWidth(150);
-            rightBar.setMinWidth(50);
-            rightBar.setMaxWidth(200);
+        GridPane rightBar = new GridPane();
+        rightBar.setPadding(new Insets(10));
+        rightBar.setPrefWidth(150);
+        rightBar.setMinWidth(50);
+        rightBar.setMaxWidth(200);
 
-            Label pollutantLabel = new Label("Choose a pollutant:");
-            ComboBox<String> pollutantComboBox = new ComboBox<>();
-            pollutantComboBox.setPromptText("Pollutant");
-            pollutantComboBox.getItems().addAll("Pm2.5", "No2", "Pm10");
+        Label pollutantLabel = new Label("Choose a pollutant:");
+        ComboBox<String> pollutantComboBox = new ComboBox<>();
+        pollutantComboBox.setPromptText("Pollutant");
+        pollutantComboBox.getItems().addAll("Pm2.5", "No2", "Pm10");
 
-            Label yearLabel = new Label("Choose a year:");
-            ComboBox<String> yearComboBox = new ComboBox<>();
-            yearComboBox.setPromptText("Year");
-            yearComboBox.getItems().addAll("2019", "2020", "2021", "2022", "2023");
+        Label yearLabel = new Label("Choose a year:");
+        ComboBox<String> yearComboBox = new ComboBox<>();
+        yearComboBox.setPromptText("Year");
+        yearComboBox.getItems().addAll("2019", "2020", "2021", "2022", "2023");
 
-            Label dataPointLabel = new Label("Value: ");
-            Label dataPointValue = new Label("select a data point");
+        Label dataPointLabel = new Label("Value: ");
+        Label dataPointValue = new Label("select a data point");
 
-            Label gridCodeLabel = new Label("Grid Code: ");
-            Label gridCodeValue = new Label("select a data point");
+        Label gridCodeLabel = new Label("Grid Code: ");
+        Label gridCodeValue = new Label("select a data point");
 
-            Label xLabel = new Label("X: ");
-            Label xValue = new Label("select a data point");
+        Label xLabel = new Label("X: ");
+        Label xValue = new Label("select a data point");
 
-            Label yLabel = new Label("Y: ");
-            Label yValue = new Label("select a data point");
+        Label yLabel = new Label("Y: ");
+        Label yValue = new Label("select a data point");
 
-            rightBar.add(pollutantLabel, 0, 0);
-            rightBar.add(pollutantComboBox, 0, 1);
-            rightBar.add(yearLabel, 0, 3);
-            rightBar.add(yearComboBox, 0, 4);
-            rightBar.add(dataPointLabel, 0, 6);
-            rightBar.add(dataPointValue, 0, 7);
-            rightBar.add(gridCodeLabel, 0, 8);
-            rightBar.add(gridCodeValue, 0, 9);
-            rightBar.add(xLabel, 0, 10);
-            rightBar.add(xValue, 0, 11);
-            rightBar.add(yLabel, 0, 12);
-            rightBar.add(yValue, 0, 13);
+        rightBar.add(pollutantLabel, 0, 0);
+        rightBar.add(pollutantComboBox, 0, 1);
+        rightBar.add(yearLabel, 0, 3);
+        rightBar.add(yearComboBox, 0, 4);
+        rightBar.add(dataPointLabel, 0, 6);
+        rightBar.add(dataPointValue, 0, 7);
+        rightBar.add(gridCodeLabel, 0, 8);
+        rightBar.add(gridCodeValue, 0, 9);
+        rightBar.add(xLabel, 0, 10);
+        rightBar.add(xValue, 0, 11);
+        rightBar.add(yLabel, 0, 12);
+        rightBar.add(yValue, 0, 13);
 
-            GridPane.setMargin(yearLabel, new Insets(10, 0, 0, 0));
-            GridPane.setMargin(yearComboBox, new Insets(0, 0, 10, 0));
-            GridPane.setMargin(dataPointLabel, new Insets(10, 0, 0, 0));
-            GridPane.setMargin(dataPointValue, new Insets(0, 0, 10, 0));
-            GridPane.setMargin(gridCodeLabel, new Insets(10, 0, 0, 0));
-            GridPane.setMargin(gridCodeValue, new Insets(0, 0, 10, 0));
-            GridPane.setMargin(xLabel, new Insets(10, 0, 0, 0));
-            GridPane.setMargin(xValue, new Insets(0, 0, 10, 0));
-            GridPane.setMargin(yLabel, new Insets(10, 0, 0, 0));
-            GridPane.setMargin(yValue, new Insets(0, 0, 10, 0));
+        GridPane.setMargin(yearLabel, new Insets(10, 0, 0, 0));
+        GridPane.setMargin(yearComboBox, new Insets(0, 0, 10, 0));
+        GridPane.setMargin(dataPointLabel, new Insets(10, 0, 0, 0));
+        GridPane.setMargin(dataPointValue, new Insets(0, 0, 10, 0));
+        GridPane.setMargin(gridCodeLabel, new Insets(10, 0, 0, 0));
+        GridPane.setMargin(gridCodeValue, new Insets(0, 0, 10, 0));
+        GridPane.setMargin(xLabel, new Insets(10, 0, 0, 0));
+        GridPane.setMargin(xValue, new Insets(0, 0, 10, 0));
+        GridPane.setMargin(yLabel, new Insets(10, 0, 0, 0));
+        GridPane.setMargin(yValue, new Insets(0, 0, 10, 0));
 
-            borderPane.setRight(rightBar);
+        borderPane.setRight(rightBar);
 
-            HBox aqiBarContainer = new HBox(10);
-            aqiBarContainer.setAlignment(Pos.CENTER);
-            aqiBarContainer.setPrefHeight(50);
-            aqiBarContainer.setMinHeight(30);
-            aqiBarContainer.setMaxHeight(80);
-            aqiBarContainer.setPadding(new Insets(10));
-            BorderPane.setMargin(aqiBarContainer, new Insets(10));
+        HBox aqiBarContainer = new HBox(10);
+        aqiBarContainer.setAlignment(Pos.CENTER);
+        aqiBarContainer.setPrefHeight(50);
+        aqiBarContainer.setMinHeight(30);
+        aqiBarContainer.setMaxHeight(80);
+        aqiBarContainer.setPadding(new Insets(10));
+        BorderPane.setMargin(aqiBarContainer, new Insets(10));
 
-            Label lowLabel = new Label("GOOD");
-            Label highLabel = new Label("POOR");
-            lowLabel.setMinWidth(Region.USE_PREF_SIZE);
-            highLabel.setMinWidth(Region.USE_PREF_SIZE);
+        Label lowLabel = new Label("GOOD");
+        Label highLabel = new Label("POOR");
+        lowLabel.setMinWidth(Region.USE_PREF_SIZE);
+        highLabel.setMinWidth(Region.USE_PREF_SIZE);
 
-            Region aqiBar = new Region();
-            aqiBar.setPrefHeight(20);
-            aqiBar.setMaxWidth(Double.MAX_VALUE);
-            aqiBar.getStyleClass().add("aqiBar");
+        Region aqiBar = new Region();
+        aqiBar.setPrefHeight(20);
+        aqiBar.setMaxWidth(Double.MAX_VALUE);
+        aqiBar.getStyleClass().add("aqiBar");
 
-            StackPane aqiStack = new StackPane();
-            aqiStack.setAlignment(Pos.CENTER);
-            HBox.setHgrow(aqiStack, Priority.ALWAYS);
-            aqiBar.prefWidthProperty().bind(aqiStack.widthProperty());
-            aqiStack.getChildren().addAll(aqiBar);
+        StackPane aqiStack = new StackPane();
+        aqiStack.setAlignment(Pos.CENTER);
+        HBox.setHgrow(aqiStack, Priority.ALWAYS);
+        aqiBar.prefWidthProperty().bind(aqiStack.widthProperty());
+        aqiStack.getChildren().addAll(aqiBar);
 
-            aqiBarContainer.getChildren().addAll(lowLabel, aqiStack, highLabel);
-            borderPane.setBottom(aqiBarContainer);
+        aqiBarContainer.getChildren().addAll(lowLabel, aqiStack, highLabel);
+        borderPane.setBottom(aqiBarContainer);
 
-            // Load data and overlay data points
-            DataLoader loader = new DataLoader();
-            DataSet dataSet = loader.loadDataFile("UKAirPollutionData/NO2/mapno22023.csv");
-            dataSet = DataFilter.filterLondonData(dataSet);
-            if (dataSet != null) {
-                int minX = Integer.MAX_VALUE;
-                int maxX = Integer.MIN_VALUE;
-                int minY = Integer.MAX_VALUE;
-                int maxY = Integer.MIN_VALUE;
+        // Load data and overlay data points
+        DataLoader loader = new DataLoader();
+        DataSet dataSet = loader.loadDataFile("UKAirPollutionData/NO2/mapno22023.csv");
+        dataSet = DataFilter.filterLondonData(dataSet);
+        if (dataSet != null) {
+            int minX = Integer.MAX_VALUE;
+            int maxX = Integer.MIN_VALUE;
+            int minY = Integer.MAX_VALUE;
+            int maxY = Integer.MIN_VALUE;
 
-                for (DataPoint dataPoint : dataSet.getData()) {
-                    if (dataPoint.x() < minX) minX = dataPoint.x();
-                    if (dataPoint.x() > maxX) maxX = dataPoint.x();
-                    if (dataPoint.y() < minY) minY = dataPoint.y();
-                    if (dataPoint.y() > maxY) maxY = dataPoint.y();
-                }
-
-                double scaleX = mapView.getFitWidth() / (maxX - minX);
-                double scaleY = mapView.getFitHeight() / (maxY - minY);
-
-                for (DataPoint dataPoint : dataSet.getData()) {
-                    double x = mapView.getX() + (dataPoint.x() - minX) * scaleX;
-                    double y = mapView.getY() + (maxY - dataPoint.y()) * scaleY;
-
-                    Circle dataPointCircle = new Circle(x, y, 5);
-                    dataPointCircle.setFill(Color.RED);
-                    dataPointCircle.setOnMouseClicked(event -> showDataPointInfo(dataPoint));
-                    anchorPane.getChildren().add(dataPointCircle);
-                }
+            for (DataPoint dataPoint : dataSet.getData()) {
+                if (dataPoint.x() < minX) minX = dataPoint.x();
+                if (dataPoint.x() > maxX) maxX = dataPoint.x();
+                if (dataPoint.y() < minY) minY = dataPoint.y();
+                if (dataPoint.y() > maxY) maxY = dataPoint.y();
             }
 
-            mapViewTab.setContent(borderPane);
+            double scaleX = mapView.getFitWidth() / (maxX - minX);
+            double scaleY = mapView.getFitHeight() / (maxY - minY);
+
+            for (DataPoint dataPoint : dataSet.getData()) {
+                double x = mapView.getX() + (dataPoint.x() - minX) * scaleX;
+                double y = mapView.getY() + (maxY - dataPoint.y()) * scaleY;
+
+                Circle dataPointCircle = new Circle(x, y, 5);
+                dataPointCircle.setFill(Color.RED);
+                dataPointCircle.setOnMouseClicked(event -> showDataPointInfo(dataPoint));
+                anchorPane.getChildren().add(dataPointCircle);
+            }
+        }
+
+        mapViewTab.setContent(borderPane);
     }
 
     private void showDataPointInfo(DataPoint dataPoint) {
--- MapImage.java
+++ MapImage.java
@@ -6,24 +6,25 @@ import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 
 public class MapImage {
-    private Image image;
-    private WritableImage writableImage;
+    private Image baseImage;
+    private WritableImage colourImage;
+    private double blendAplha = 0.5;
 
     public MapImage(String fileName) {
         try {
             FileInputStream input = new FileInputStream(fileName);
-            image = new Image(input);
+            baseImage = new Image(input);
 
-            writableImage = new WritableImage(
-                    (int)image.getWidth(),
-                    (int)image.getHeight()
+            colourImage = new WritableImage(
+                    (int)baseImage.getWidth(),
+                    (int)baseImage.getHeight()
             );
 
-            PixelReader reader = image.getPixelReader();
-            PixelWriter writer = writableImage.getPixelWriter();
-            for (int y = 0; y < image.getHeight(); y++) {
-                for (int x = 0; x < image.getWidth(); x++) {
-                    writer.setArgb(x, y, reader.getArgb(x, y));
+            PixelWriter writer = colourImage.getPixelWriter();
+            int blankArgb = 0xFF << 24;
+            for (int y = 0; y < baseImage.getHeight(); y++) {
+                for (int x = 0; x < baseImage.getWidth(); x++) {
+                    writer.setArgb(x, y, blankArgb);
                 }
             }
 
@@ -33,45 +34,84 @@ public class MapImage {
     }
 
     public Image getImage() {
-        return writableImage;
+        return baseImage;
     }
 
-    private void makePixelRedder(int x, int y, double percentage){
-        int[] rgb = getPixel(x, y);
-        int red = Math.min(255, (int)(rgb[0] * (1 + percentage)));
-        int green = rgb[1];
-        int blue = rgb[2];
-        int argb = (0xFF << 24) | (red << 16) | (green << 8) | blue;
-        writableImage.getPixelWriter().setArgb(x, y, argb);
+    private void placeOverlayBlock(int startX, int startY, int width, int height, double dataPercentage) {
+        PixelWriter writer = colourImage.getPixelWriter();
+        int alpha = 255;
+        int green = (int)(255 * (1 - dataPercentage));
+        int red = (int)(255 * dataPercentage);
+        int blue = 0;
+        int argb = (alpha << 24) | (red << 16) | (green << 8) | blue;
+        for (int y = startY; y < startY + height; y++) {
+            for (int x = startX; x < startX + width; x++) {
+                writer.setArgb(x, y, argb);
+            }
+        }
     }
 
     public void testPixelAltering() {
-        for (int y = 0; y < writableImage.getHeight() - 500; y++) {
-            for (int x = 0; x < writableImage.getWidth() - 400; x++) {
-                makePixelRedder(x, y, 0.9);
-            }
-        }
-        for (int y = 0; y < writableImage.getHeight() - 300; y++) {
-            for (int x = 0; x < writableImage.getWidth() - 300; x++) {
-                makePixelRedder(x, y, 0.7);
-            }
-        }
-        for (int y = 0; y < writableImage.getHeight() - 200; y++) {
-            for (int x = 0; x < writableImage.getWidth() - 100; x++) {
-                makePixelRedder(x, y, 0.5);
-            }
-        }
-
+        placeOverlayBlock(0, 0, 100, 100, 0.0);
+        placeOverlayBlock(100, 100, 100, 100, 0.1);
+        placeOverlayBlock(200, 200, 100, 100, 0.2);
+        placeOverlayBlock(300, 300, 100, 100, 0.3);
+        placeOverlayBlock(400, 400, 100, 100, 0.4);
+        placeOverlayBlock(500, 500, 100, 100, 0.5);
+        placeOverlayBlock(600, 600, 100, 100, 0.6);
+        placeOverlayBlock(700, 700, 100, 100, 0.7);
+        placeOverlayBlock(800, 800, 100, 100, 0.8);
+        placeOverlayBlock(900, 900, 100, 100, 0.9);
+        placeOverlayBlock(1000, 1000, 100, 100, 1.0);
     }
 
     public int[] getPixel(int x, int y) {
-        PixelReader reader = writableImage.getPixelReader();
+        PixelReader reader = colourImage.getPixelReader();
         int argb = reader.getArgb(x, y);
         int[] rgb = new int[3];
         rgb[0] = (argb >> 16) & 0xFF;
         rgb[1] = (argb >> 8) & 0xFF;
         rgb[2] = argb & 0xFF;
         return rgb;
+    }
+
+    public Image getCombined() {
+
+        WritableImage newImage = new WritableImage((int) baseImage.getWidth(), (int) baseImage.getHeight());
+        int width = (int) Math.min(baseImage.getWidth(), colourImage.getWidth());
+        int height = (int) Math.min(baseImage.getHeight(), colourImage.getHeight());
+
+        PixelReader overlayReader = colourImage.getPixelReader();
+        PixelReader baseReader = baseImage.getPixelReader();
+        PixelWriter writer = newImage.getPixelWriter();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int baseArgb = baseReader.getArgb(x, y);
+                int overlayArgb = overlayReader.getArgb(x, y);
+
+                int baseRed = (baseArgb >> 16) & 0xFF;
+                int baseGreen = (baseArgb >> 8) & 0xFF;
+                int baseBlue = baseArgb & 0xFF;
+
+                int overlayRed = (overlayArgb >> 16) & 0xFF;
+                int overlayGreen = (overlayArgb >> 8) & 0xFF;
+                int overlayBlue = overlayArgb & 0xFF;
+
+                int newRed = (int) (baseRed * (1 - blendAplha) + overlayRed * blendAplha);
+                int newGreen = (int) (baseGreen * (1 - blendAplha) + overlayGreen * blendAplha);
+                int newBlue = (int) (baseBlue * (1 - blendAplha) + overlayBlue * blendAplha);
+
+                newRed = Math.min(255, Math.max(0, newRed));
+                newGreen = Math.min(255, Math.max(0, newGreen));
+                newBlue = Math.min(255, Math.max(0, newBlue));
+
+                int newArgb = (0xFF << 24) | (newRed << 16) | (newGreen << 8) | newBlue;
+                writer.setArgb(x, y, newArgb);
+            }
+        }
+
+        return newImage;
     }
     }
 
